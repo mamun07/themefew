d10da26d7c1ea692e0b2031078d2a4361a22939f
mamun07 <mamunben7@gmail.com>
2020-12-13 15:46:39 +0600

    Font-awesome add complete

--- themefew/src/pages/TobBar.jsx
+++ themefew/src/pages/TobBar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import {Container, Row, Col, Navbar,Nav} from 'react-bootstrap'
 
+import {Link} from 'react-router-dom'
 
 
 export default class TobBar extends Component {
@@ -10,14 +10,22 @@ export default class TobBar extends Component {
             <>
                 <Container fluid className="header-topbar">
                     <Row>
-                        <Col lg={6}>
-                            Left site
+                        <Col lg={3}>
+                            <div className="topbar-left-navbar"><spna>Phone : +0510-5454545</spna></div>
                         </Col>
-                        <Col lg={6}>
-                            <div className="float-right">
-                                <ul>
-                                    <li><Link exact to="/help">Help</Link></li>
-                                </ul>
+                        <Col lg={5}>
+                            <div className="topbar-center-navbar text-center">
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, quasi.</p>
+                            </div>
+                        </Col>
+                        <Col lg={4}>
+                            <div className="topbar-right-navbar">
+                                <Navbar>
+                                    <Nav className="ml-auto">
+                                        <Link to="/about">Help</Link>
+                                        <Link to="/">Login</Link>
+                                    </Nav>
+                                </Navbar>
                             </div>
                         </Col>
                     </Row>
